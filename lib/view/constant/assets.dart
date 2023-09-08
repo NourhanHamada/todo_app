5af184c9349b3552d8536ca590c55251168a5968
NourhanHamada <nourhan.hamada996@gmail.com>
2023-09-08 09:25:43 +0300

    add colors & assets

---
+++ lib/view/constant/assets.dart
@@ -0,0 +1,8 @@
+// Logo
+
+// Images
+const String splashImage = 'assets/images/splash.svg';
+const String todoIll = 'assets/images/todo_ill.svg';
+
+// Icons
+const String arrowBack = 'assets/icons/arrow_back.svg';
---
+++ lib/view/constant/colors.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+const Color mainColor = Color(0xff55847A);
+Color mainColorWithOpacity = const Color(0xff55847A).withOpacity(.4);
+const Color backgroundColor = Color(0xffF5F5F5);
